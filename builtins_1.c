f6015d05058d038b697f9d7fdc368ec87f1da918
JwowSquared <Jdwagner1@hotmail.com>
2020-08-08 04:07:51 +0000

    added documentation, updated most builtins to not use _getenv, and instead use getenv_value or just findenvi

--- builtins_1.c
+++ builtins_1.c
@@ -1,6 +1,11 @@
 #include "header_shell.h"
 /**
-* exit_shell - [FIXME]
+* exit_shell - cleans up memory and exits program with status
+* @t: token with arguments
+* @buffer: buffer to free
+* @envc: environment variables to free
+*
+* Return: always 1, but should never return
 */
 int exit_shell(token *t, char **buffer, char ***envc)
 {
@@ -15,7 +20,12 @@ int exit_shell(token *t, char **buffer, char ***envc)
 }
 
 /**
-* _env - [FIXME]
+* env_shell - prints environment variables
+* @t: not needed in this function
+* @buffer: not needed in this function
+* @envc: environment variables to print
+*
+* Return: always 1
 */
 int env_shell(token *t, char **buffer, char ***envc)
 {
@@ -34,7 +44,12 @@ int env_shell(token *t, char **buffer, char ***envc)
 }
 
 /**
-* _setenv - [FIXME]
+* setenv_shell - updates env on match, else create new env
+* @t: token with arguments
+* @buffer: not needed in this function
+* @envc: environment variables
+*
+* Return: always 1
 */
 int setenv_shell(token *t, char **buffer, char ***envc)
 {
@@ -49,45 +64,49 @@ int setenv_shell(token *t, char **buffer, char ***envc)
 	}
 
 	i = findenvi(t->arguments[1], *envc);
-	if ((*envc)[i] == NULL)
+	if (i == -1)
 		*envc = copy_aos(envc, _strcat(t->arguments[1], t->arguments[2], '='));
 	else
-	{
-		free((*envc)[i]);
-		(*envc)[i] = _strcat(t->arguments[1], t->arguments[2], '=');
-	}
+		update_env(t->arguments[1], t->arguments[2], envc);
 
 	return (1);
 }
 
 /**
-* findenvi - [FIXME]
-* @name: the name of the ev that you are searching for
-* @envp: ev master list
-* Return: pointer to string of requested ev
+* findenvi - finds index of key in envc
+* @key: key to match with
+* @envc: environment variables
+*
+* Return: index of env in envc, else -1
 */
-int findenvi(const char *name, char **envc)
+int findenvi(char *key, char **envc)
 {
 	int i = 0, j = 0;
 
 	while (envc[i])
 	{
-		while (name[j])
+		while (key[j])
 		{
-			if (name[j] != envc[i][j])
+			if (key[j] != envc[i][j])
 				break;
 			j++;
 		}
-		if (name[j] == '\0')
+		if (key[j] == '\0')
 			return (i);
 		i++;
 	}
-	return (i);
+	/* return -1 if key doesn't exist in envc */
+	return (-1);
 }
 
 /**
- *
- */
+* unsetenv_shell - deletes env in envc
+* @t: token of arguments
+* @buffer: unused in this function
+* @envc: environment variables
+*
+* Return: always 1
+*/
 int unsetenv_shell(token *t, char **buffer, char ***envc)
 {
 	int i;
@@ -101,21 +120,16 @@ int unsetenv_shell(token *t, char **buffer, char ***envc)
 	}
 
 	i = findenvi(t->arguments[1], *envc);
-	if ((*envc)[i] == NULL)
-	{
-		perror("Could not find variable");
+	if (i == -1)
 		return (1);
-	}
-	else
+
+	free((*envc)[i]);
+	(*envc)[i] = (*envc)[i + 1];
+	i++;
+	while ((*envc)[i])
 	{
-		free((*envc)[i]);
 		(*envc)[i] = (*envc)[i + 1];
 		i++;
-		while((*envc)[i])
-		{
-			(*envc)[i] = (*envc)[i + 1];
-			i++;
-		}
 	}
 
 	return (1);
