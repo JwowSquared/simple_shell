61b7b246282c5b61bb58136944bc9b997ad4a7de
JwowSquared <Jdwagner1@hotmail.com>
2020-08-10 18:12:24 +0000

    added unsetenv

--- env.c
+++ env.c
@@ -3,6 +3,32 @@
 /**
 *
 */
+int update_env(char *key, char *value, db_t *db)
+{
+	char **temp, *pair;
+	int i = find_env(key, db);
+
+	if (key == NULL || value == NULL)
+		return (0);
+	if (i == -1)
+	{
+		pair = _strcat(key, "=", value);
+		temp = copy_aos(db->envc, pair);
+		free(pair);
+		free_aos(&db->envc);
+		db->envc = temp;
+	}
+	else
+	{
+		free(db->envc[i]);
+		db->envc[i] = _strcat(key, "=", value);
+	}
+	return (1);
+}
+
+/**
+*
+*/
 char *getenv_value(char *key, db_t *db)
 {
 	int len = 0, i = find_env(key, db);
