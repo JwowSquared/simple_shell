8e82d444741bf0c0b6466a163179d73a3758d79d
JHS1790 <1790@holbertonschool.com>
2020-08-12 14:12:33 +0000

    Added comments to create_token and filled in missing documentation where needed and a few minor betty checks

--- create_token.c
+++ create_token.c
@@ -1,22 +1,23 @@
 #include "header_shell.h"
 
 /**
-* DOCUMENTATION
-*/
+ * create_token - create a command token for execution
+ * @buffer: buffer of command and arguments passed to create_token
+ * Return: pointer to token struct after parsing of original buffer
+ */
 token *create_token(char **buffer)
 {
-
 	int len = 0, i = 0, j = 0, argc, argi = 0;
 	token *out;
 
 	argc = count_arguments(buffer);
 	if (argc == -1)
 		return (NULL);
-
 	out = malloc(sizeof(token));
 	if (out == NULL)
 		return (NULL);
 
+	/*incrementing through leading spaces, recommend a spaces incrementation function*/
 	while ((*buffer)[i + len] != '\n')
 	{
 		if ((*buffer)[i + len] == ' ')
@@ -29,24 +30,29 @@ token *create_token(char **buffer)
 		else
 			len++;
 	}
+	/*end incrementing*/
 
 	out->command = malloc(sizeof(char) * len);
 	if (out->command == NULL)
 		return (NULL);
 
+	/*assigning out->command, maybe not necessary?*/
 	while (j < len)
 	{
 		out->command[j] = (*buffer)[i + j];
 		j++;
 	}
-	i += j;
+	i += j; /*<- incrementing i with j probably not necesary, as we want the command to be in out->arguments*/
 	out->argc = argc;
 	out->arguments = malloc(sizeof(char *) * (argc + 1));
 	if (out->arguments == NULL)
 		return (NULL);
+
+	/*if we don't increment i with j, then we won't need the while loop*/
 	while ((*buffer)[i] == ' ')
 		i++;
 
+	/*setting the arguments, reccomend this whole loop be made its own function, something like void set_args(token *, int index)*/
 	while (argc > 0)
 	{
 		len = 0;
@@ -71,14 +77,17 @@ token *create_token(char **buffer)
 		argi++;
 		argc--;
 	}
+	/*end setting arguments*/
 
 	out->arguments[argi] = NULL;
 	return (out);
 }
 
 /**
-* DOCUMENTATION
-*/
+ * count_arguments - counts the number of arguments from the given buffer
+ * @buffer: buffer containing command and arguments passed to count_arguments
+ * Return: number (int) of arguments determined from buffer
+ */
 int count_arguments(char **buffer)
 {
 	int out = 0, i = 0;
